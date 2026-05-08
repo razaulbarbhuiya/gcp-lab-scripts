6e66b9e352f25e92cad090776e91362a799bfa65
BLAZE 🚀 <107280540+razaulbarbhuiya@users.noreply.github.com>
2026-05-09 00:54:31 +0530

    Add script to set up Google Cloud Scheduler

--- gsp401.sh
+++ gsp401.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+if [ -z "$REGION" ]; then
+REGION="us-central1"
+fi
+
+gcloud config set compute/region $REGION
+
+gcloud services enable cloudscheduler.googleapis.com
+
+gcloud pubsub topics create cron-topic
+
+gcloud pubsub subscriptions create cron-sub \
+--topic cron-topic
+
+gcloud scheduler jobs create pubsub cron-job \
+--schedule="* * * * *" \
+--topic=cron-topic \
+--message-body="hello cron!" \
+--location=$REGION
+
+sleep 90
+
+gcloud pubsub subscriptions pull cron-sub \
+--limit=5 \
+--auto-ack
+
+echo "Lab Completed"
